638932340c6a2687ae1443c39491a91fcb868d70
Tobias Schönberg <tobias47n9e@gmail.com>
2017-11-10 14:21:15 +0100

    Fix showing of panel of plugin settings

--- src/settings/SuluSettings.java
+++ src/settings/SuluSettings.java
@@ -3,15 +3,20 @@ package settings;
 import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.project.Project;
-import com.intellij.psi.PsiManager;
 import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SuluSettings implements Configurable {
-    protected Project project;
     private JPanel panel;
+    private JCheckBox enablePluginCheckBox;
+    private List<String> settings = new ArrayList<String>();
 
     @Nullable
     @Override
@@ -22,16 +27,25 @@ public class SuluSettings implements Configurable {
     @Nls
     @Override
     public String getDisplayName() {
-        return "Sulu";
+        return "Sulu Plugin";
     }
 
     @Override
     public void apply() throws ConfigurationException {
-        PsiManager.getInstance(project).dropResolveCaches();
+
     }
 
     @Override
     public boolean isModified() {
         return false;
+    }
+
+    public SuluSettings(@NotNull final Project project) {
+        enablePluginCheckBox.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent event) {
+                boolean enabled = event.getStateChange() == ItemEvent.SELECTED;
+                enablePluginCheckBox.setEnabled(enabled);
+            }
+        });
     }
 }
